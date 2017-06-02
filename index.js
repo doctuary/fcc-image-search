cc49b8598d31acc9d4db4e6f201808f212339088
bloctuary <doctuary@github.com>
2017-06-02 07:17:11 -0500

    Add a /:search endpoint

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/', function(req, res) {
     res.send('This is an image search abstraction layer');
 });
 
+app.get('/:search', function(req, res) {
+    res.send('You found the search endpoint.');
+});
+
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port ', app.get('port'));
 });
