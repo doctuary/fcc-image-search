7872603840643d6dc7735244d2e747437fcbf744
bloctuary <doctuary@github.com>
2017-06-03 07:54:03 -0500

    Try inserting search results into database

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 const GoogleImages = require('google-images');
 
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
+var url = process.env.MONGOLAB_URI;
+
 const searchClient = new GoogleImages('013847059286481252061:qhhqalqaddm', 'AIzaSyD8xxoiT4DP4l9KYnO7RK4LUQHtUbn4KOA');
 
 var app = express();
@@ -12,9 +16,26 @@ app.get('/', function(req, res) {
 });
 
 app.get('/:search', function(req, res) {
+    let searchValue = req.params.search;
     let page = req.query.offset ? req.query.offset : 1;
-    searchClient.search(req.params.search, {page: page})
-	.then(function(images) {res.send(images)});
+    searchClient.search(searchValue, {page: page})
+	.then(function(images) {
+        MongoClient.connect(url, function(err, db) {
+            if (err) {
+                return console.log('Unable to connect to the mongoDB server. Error:', err);
+            } else {
+                let imageSearches = db.collection('imageSearches');
+                let result = {
+                    searchValue: searchValue,
+                    searchResult: images
+                }
+                imageSearches.insert(result, function(){
+                    db.close();
+                    res.send(images);
+                })
+            }
+        });
+    });
 });
 
 app.listen(app.get('port'), function() {
