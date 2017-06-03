0201008b91664049bb4d1b9f257b465ba29fce02
bloctuary <doctuary@github.com>
2017-06-03 07:37:39 -0500

    Improve instructions on landing page

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(req, res) {
-    res.send('This is an image search abstraction layer');
+    res.send('This is an image search abstraction layer. Add your search string after the domain (ex. .../my search string).');
 });
 
 app.get('/:search', function(req, res) {
