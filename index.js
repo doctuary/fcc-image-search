c775e5503171392d3ed26433b1d6b036d02ac890
bloctuary <doctuary@github.com>
2017-06-03 08:07:24 -0500

    Expand instructions

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(req, res) {
-    res.send('This is an image search abstraction layer. Add your search string after the domain (ex. .../my search string).');
+    res.send('This is an image search abstraction layer. Add your search string after the domain (ex. .../api/searchFor/my search string). Get recent searches with /api/recentSearches');
 });
 
 app.get('/api/searchFor/:search', function(req, res) {
